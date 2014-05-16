366249a7933aad6eee9ee8519dc5f050e0700064
grahambyers <graham.byers@gmail.com>
2014-05-16 15:54:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+        var fname = document.getElementById('fname').value;
+        var lname = document.getElementById('lname').value;
+        var age = document.getElementById('age').value;
+        
+        if (fname.length == 0) {
+            alert("Please input a first name");
+        } else if (lname.length == 0) {
+            alert("Please input a last name");
+        } else if (age.length == 0) {
+            alert("Please input an age");
+        }
+    });
+});
