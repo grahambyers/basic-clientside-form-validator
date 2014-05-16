62cd31baa1b5f639ba86d268c0b6dbb439ef4d12
grahambyers <graham.byers@gmail.com>
2014-05-16 16:20:17 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+    $("#submit").click(function () {
+        var fname = document.getElementById('fname').value;
+        var lname = document.getElementById('lname').value;
+        var age = document.getElementById('age').value;
+        
+        if (fname.length == 0) {
+            alert("Please input a first name");
+        } else if (lname.length == 0) {
+            alert("Please input a last name");
+        } else if (age.length == 0) {
+            alert("Please input an age");
+        } else {
+            document.getElementById("answer").innerHTML = ("Hello " + fname + " " + lname + ", you are " + age);
+        }
+        
+    });
+});
